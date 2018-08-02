50df29b9c67b5fae1b693f9f9f4523cc1cc385d3
Ken-Crowley <kdcrowle@asu.edu>
2018-08-02 14:22:03 -0700

    Displays Smoothing of the Image-based domain parameter

---
+++ 2D_Diffusion_SBM/Plot_FORTRAN_Diffusion_Smoothing.m
@@ -0,0 +1,25 @@
+clc
+clear
+
+% Reads in concentration dat file created in FORTRAN
+A = ReadArray_FortranBinary('DP_Smoothed_Animate.dat',3);
+
+[time, M, N] = size(A);
+
+% Makes gif
+gif('DP_Smoothing.gif')
+
+for i = 1:time
+    rshape = reshape(A(i,:,:),[M N]);
+    pcolor(rshape)
+    hold on;
+    colormap jet;
+    shading flat;
+    axis equal;
+    hold off;
+    pause(0.001);
+    gif
+end
+
+web('DP_Smoothing.gif');
+
