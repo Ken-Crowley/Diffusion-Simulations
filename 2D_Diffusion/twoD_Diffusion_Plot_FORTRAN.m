4f18ca39307aef0b1b089f8d5ca2e4c66f6227f9
Ken-Crowley <42042876+Ken-Crowley@users.noreply.github.com>
2018-08-02 12:34:43 -0700

    Update twoD_Diffusion_Plot_FORTRAN.m
    Added gif maker

--- 2D_Diffusion/twoD_Diffusion_Plot_FORTRAN.m
+++ 2D_Diffusion/twoD_Diffusion_Plot_FORTRAN.m
@@ -1,15 +1,21 @@
 clc
 clear
 
+% Reads in concentration dat file created in FORTRAN
 A = ReadArray_FortranBinary('concdata.dat',3);
 
+% Time parameters
 t0 = 0;
 tf = 5;
 dt = .0003;
 time = t0:dt:tf;
 
+% Grid size
 Nx = 100;
 Ny = 100;
+
+% Makes gif
+gif('test.gif')
 
 for i = 1:100:length(time)
     rshape = reshape(A(i,:,:),[Nx Ny]);
@@ -20,5 +26,8 @@ for i = 1:100:length(time)
     axis equal;
     hold off;
     pause(0.001);
+    gif
 end
+
+web('test.gif');
 
